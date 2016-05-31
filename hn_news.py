3b68483a2cf0b439d0728e6d7a48b74247ced534
mayank26saxena <mayank26saxena@gmail.com>
2016-05-31 19:34:15 +0530

    new menu implemented.

--- hn_news.py
+++ hn_news.py
@@ -2,16 +2,24 @@ import os
 import signal
 import json
 import webbrowser
+import pygtk
+import gtk
 from urllib2 import Request, urlopen, URLError
-from gi.repository import Gtk as gtk
-from gi.repository import AppIndicator3 as appindicator
-from gi.repository import Notify as notify
+#from gi.repository import Gtk as gtk
+#from gi.repository import AppIndicator3 as appindicator
+import appindicator 
+#from gi.repository import Notify as notify
+import pynotify
 from hackernews import HackerNews
 
 
 #KEYWORDS
-APPINDICATOR_ID = 'hn-news-indicator'
+APPINDICATOR_ID = 'hn-indicator'
+PYNOTIFY_ID = 'hn-indicator'
 LIMIT = 3
+ONE_MINUTE = 1000*60
+DURATION = 30
+REFRESH_DURATION = ONE_MINUTE*DURATION
 SPACE = ' '
 DASH = '-'
 POINTS = 'Points'
@@ -25,14 +33,14 @@ news = ['?']*LIMIT
 item = ['?']*LIMIT
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('hackernews.png'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
-    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(build_menu())
-    notify.init(APPINDICATOR_ID)
+    indicator = appindicator.Indicator(APPINDICATOR_ID, os.path.abspath('hackernews.png'), appindicator.CATEGORY_COMMUNICATIONS)
+    indicator.set_status(appindicator.STATUS_ACTIVE)
+    indicator.set_menu(build_menu(LIMIT))
+    pynotify.init(APPINDICATOR_ID)
     gtk.main()
-    gtk.timeout_add(5000, update_widget)
+    #gtk.timeout_add(5000, update_widget)
     
-def build_menu():
+def build_menu(LIMIT):
     menu = gtk.Menu()
 
     get_data()
@@ -50,6 +58,17 @@ def build_menu():
     separator = gtk.SeparatorMenuItem()
     menu.append(separator)
 
+    item_change_refresh_duration = gtk.MenuItem('Change Refresh Duration')
+    item_change_refresh_duration.connect('activate', get_refesh_duration)
+    menu.append(item_change_refresh_duration)
+
+    item_change_no_in_feed = gtk.MenuItem('Change number of items displayed in list')
+    item_change_no_in_feed.connect('activate', get_feed_entry_number)
+    menu.append(item_change_no_in_feed)
+
+    separator2 = gtk.SeparatorMenuItem()
+    menu.append(separator2)
+
     item_refresh = gtk.MenuItem('Refresh')
     item_refresh.connect('activate' , refresh)
     menu.append(item_refresh)
@@ -62,7 +81,8 @@ def build_menu():
     return menu
 
 def quit(_):
-    notify.uninit()
+    #notify.uninit()
+    show_notification(None, "Quitting", "Exiting from hn-indicator.")
     gtk.main_quit()
 
 def get_data():
@@ -81,16 +101,61 @@ def get_data():
 def open_url(widget,url):
     webbrowser.open(url)
 
-def show_notification(title, message):
-    notify.Notification.new(title, message, None).show()
+#def show_notification(title, message):
+#    notify.Notification.new(title, message, None).show()
 
-def refresh(widget):
+def show_notification(self, title="Title", msg="Message", timeout=1000):
+		notification = pynotify.Notification(title, msg)
+		notification.set_timeout(timeout)
+		notification.show()
+
+def refresh(widget, limit):
     ids[:] = []
     titles[:] = []
-    points[:] = []
+    points[:] = []  
     urls[:] = []
-    build_menu()
+    build_menu(limit)
     show_notification("Hacker News Widget updated!", "Click on the widget to view updated news feed.")
+
+
+def get_refesh_duration(widget):
+    DURATION = get_dialog_entry(None, "Enter duration after which feed should be refreshed (in mins): ")
+    REFRESH_DURATION = DURATION*ONE_MINUTE
+    print 'New refresh duration is - ' + REFRESH_DURATION
+    show_notification(widget, "Settings updated!", "Successfully changed the refresh duration settings.")
+
+def get_feed_entry_number(widget):
+    LIMIT = get_dialog_entry(None, "Enter number of list items in feed (<10): ")
+    print 'New limit is - ' + str(LIMIT)
+    show_notification(widget, "Settings updated!", "Successfully changed the number of items in feed settings.")
+    refresh(widget, LIMIT)
+
+def get_dialog_entry(parent, message, default=''):
+    """
+    Display a dialog with a text entry.
+    Returns the text, or None if canceled.
+    """
+    d = gtk.MessageDialog(parent,
+                          gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                          gtk.MESSAGE_QUESTION,
+                          gtk.BUTTONS_OK_CANCEL,
+                          message)
+    entry = gtk.Entry()
+    entry.set_text(default)
+    entry.show()
+    d.vbox.pack_end(entry)
+    entry.connect('activate', lambda _: d.response(gtk.RESPONSE_OK))
+    d.set_default_response(gtk.RESPONSE_OK)
+
+    r = d.run()
+    text = entry.get_text().decode('utf8')
+    d.destroy()
+    if r == gtk.RESPONSE_OK:
+        return text
+    else:
+        return None
+
+
 
 if __name__ == "__main__" :
     signal.signal(signal.SIGINT, signal.SIG_DFL)
